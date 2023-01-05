750cfb0c052ccbb4014fbb54de5ef7255b97d182
Polina Tikhonova <polinochkatikhonova@gmail.com>
2023-01-06 02:17:36 +0300

    new file

---
+++ presentation-maker/src/components/slideObject/SlideObject.tsx
@@ -0,0 +1,12 @@
+import { SlideObjectProps } from "../../types/SlideObjectType"
+
+
+function Object(prop: SlideObjectProps) {
+    return (
+        <rect x="100" y="100" width="200" height="100"/>
+    )
+}
+
+export {
+    Object
+}
