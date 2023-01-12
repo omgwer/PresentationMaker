c5a0a8b5234ed736d1d542a0dce03341fc3f590d
Dudin Alexey <dudin.lesha@gmail.com>
2023-01-12 09:19:13 +0300

    - начало экспорта в PDF

--- presentation-maker/src/functions/PdfExporter.ts
+++ presentation-maker/src/functions/PdfExporter.ts
@@ -1,14 +1,28 @@
-import { Presentation } from "../types/PresentationType";
-import { Template, BLANK_PDF } from '@pdfme/generator';
+import {Presentation} from "../types/PresentationType";
+import pdfkit from "pdfkit"
 
-function getSlides():any {
+function getSlides(): any {
     return {}
 }
+
 export function GeneratePdf(presentation: Presentation) {
-    let template:Template = {
-        basePdf:BLANK_PDF,
-        schemas: [
-            getSlides()
-        ]
-    }
+    // require dependencies
+    //let PDFDocument = require('pdfkit');
+    let blobStream = require('blob-stream');
+    // create a document the same way as above
+    let doc = new pdfkit;
+    // pipe the document to a blob
+    let stream = doc.pipe(blobStream());
+    // add your content to the document here, as usual
+    doc.addPage();
+    // get a blob when you're done
+    doc.end();
+    stream.on('finish', function () {
+        // get a blob you can do whatever you like with
+        const blob = stream.toBlob('application/pdf')
+        let link = document.createElement("a");
+        link.setAttribute("href", URL.createObjectURL(blob));
+        link.setAttribute("download", presentation.name + ".json");
+        link.click();
+    });
 }
